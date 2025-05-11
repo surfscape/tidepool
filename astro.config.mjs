df575addfb1d2e9904508169393ec4e1f5063c1b
ReduxFlakes <87225951+ReduxFlakes@users.noreply.github.com>
2025-05-11 02:16:23 +0100

    initial release

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,22 +4,38 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://surfscape.github.io/tidepool',
+	site: 'https://surfscape.github.io',
+	base: 'tidepool',
 	integrations: [
 		starlight({
 			title: 'Tidepool',
+			logo: {
+				src: "/src/assets/icon.svg"
+			},
+			favicon: "/icon.svg",
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/surfscape/' }],
 			editLink: {
-				baseUrl: 'https://github.com/surfscape/tidepool/edit/main/docs/',
+				baseUrl: 'https://github.com/surfscape/tidepool/edit/main/',
 			},
 			sidebar: [
 				{
 					label: "Hallway",
 					items: [
 						"introduction",
-						"contribute"
+						"faq",
+						"contribute",
+						"coc",
+						"whats-new"
 					]
 				},
+				{
+					label: "Projects",
+					autogenerate: { directory: "projects" }
+				},
+				{
+					label: "SurfScape",
+					autogenerate: { directory: "surfscape" }
+				}
 			],
 			credits: true
 		}),
---
+++ src/content/docs/whats-new.md
@@ -0,0 +1,8 @@
+---
+title: What's New
+description: History of changes made to Tidepool
+---
+
+## 11, May 2025
+
+- Kickstarted Tidepool!
